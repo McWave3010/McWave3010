e27775b34b31e033f27c9f45a87f35a26c7b3a8f
McWave3010 <150445453+McWave3010@users.noreply.github.com>
2024-05-17 18:33:13 +0000

    updated

--- main_page.js
+++ main_page.js
@@ -10,19 +10,43 @@ document.addEventListener('DOMContentLoaded', function(){
         document.querySelector('.choose-options').style.display = 'none';
         document.querySelector('.signing-buttons').style.display = 'none';
         document.querySelector('.manager-sign-up-page').style.display = 'flex';
-    
-        //function to capitalize inputs 
-        
-      function form() {
-        let inputted = document.querySelector(".inputted_4");
-        for (let x = 0; x<inputted.lenght;x++){
-            if (inputted[x] === "text"){
-                return x.toUpperCase();
-            }
-        }
-        console.log(form());
-      }
 
     }
+    //Stating students with homepage and student sign up
+    // document.querySelector("#student-button").onclick = function() {
+    //     document.querySelector(".main").style.display = 'none';
+    //     document.querySelector(".students-main-div").style.display = 'flex';
+    //     document.querySelector(".links-lists").style.display = 'flex';
+    //     document.querySelector("#lists-of-links").style.display = 'flex';
+    //     document.querySelector(".gridding-pictures").style.display = 'flex';
+    //     document.querySelector(".main-gridding-container").style.display = 'grid';
+    //}
+    document.querySelector("#signer-sign-in").onclick = function() {
+        document.querySelector(".main").style.display = 'flex';
+        document.querySelector(".students-main-div").style.display = 'none';
+        document.querySelector(".links-lists").style.display = 'none';
+        document.querySelector("#lists-of-links").style.display = 'none';
+        document.querySelector(".gridding-pictures").style.display = 'none';
+        document.querySelector(".main-gridding-container").style.display = 'none';
+    
+}
+document.querySelector("#linker").onclick = function() {
+    document.querySelector("#svg-menu").style.display = 'none';
+    document.querySelector(".navbar-nav").style.display = 'grid';
+
+}
+    
+document.querySelector("#svg-close").onclick = function() {
+    document.querySelector(".navbar-nav").style.display = 'none';
+    document.querySelector("#svg-menu").style.display = 'flex';
+  
+}
+document.querySelector("#signer-signing-in").onclick = function() {
+    document.querySelector(".students-main-div").style.display = 'none';
+    document.querySelector(".gridding-pictures").style.display = 'none';
+    document.querySelector(".about-section").style.display = 'none';
+    document.querySelector(".main").style.display = 'flex';
+}
+
 }
 )
